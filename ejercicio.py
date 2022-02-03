339900e65ffd854a9a1b77e49cc9e3b389a5fae0
FanorValencia <fjunnior517@gmail.com>
2022-02-03 03:01:56 +0100

    Maximos de Matriz

--- ejercicio.py
+++ ejercicio.py
@@ -11,5 +11,11 @@ for i in range (row):
 #print (matriz)
 
 m2 = [[i*cols+j for j in range (cols)] for i in range (row)]
-
 print (m2)
+
+def maximo (lista):
+    return max(lista)
+
+elem_maximos = list(map(maximo,m2))
+
+print ('Los elementos mayores son: ', elem_maximos)
